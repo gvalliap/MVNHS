f622f7d6dc8752a7933e56785de3e1c6f86c9d07
Ganesh V <ganeshv757@gmail.com>
2015-11-28 19:11:05 -0800

    animated typing

--- home.php
+++ home.php
@@ -4,123 +4,17 @@
         <?php include("head.php"); ?>
     </head>
     <body>
-        <div id="home-navbar" class="navbar-fixed">
-            <nav>
-                <div class="nav-wrapper light-blue darken-3">
-                    <a href="#" class="brand-logo center">Monta Vista NHS</a>
-                    <a href="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></a>
-                    <ul class="right hide-on-small-only">
-                        <li><a class="waves-effect waves-light modal-trigger" href="#register"><i class="material-icons left">group</i>Register</a></li>
-                        <li><a class="waves-effect waves-light modal-trigger" href="#login"><i class="material-icons left">send</i>Login</a></li>
-                    </ul>
-                    <ul id="mobile-nav" class="side-nav">
-                        <li><a href="register.html"><i class="material-icons left">group</i>Register</a></li>
-                        <li><a class="modal-trigger" href="#login"><i class="material-icons left">send</i>Login</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
+        <header>
 
-        <div id="home-banner" class="parallax-container">
-            <div class="parallax"><img src="https://wallpaperscraft.com/image/crowd_people_dance_silhouette_57172_3840x2160.jpg"></div>
-        </div>
-
-        <div class="container">
-            <div class="section">
-                <div class="row">
-                    <div class="col s12 l3 white">
-                        <div class="icon-block">
-                            <h2 class="center red-text"><i class="material-icons large">perm_identity</i></h2>
-                            <h4 class="center red white-text col-content">Character</h4>
-                            <p>
-                                Barton did feebly change man she afford square add. Want eyes by neat so just must. Past draw tall up face show rent oh mr. Required is debating extended wondered as do. New get described applauded incommode shameless out extremity but. Resembled at perpetual no believing is otherwise sportsman. Is do he dispatched cultivated travelling astonished. Melancholy am considered possession on collecting everything.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col s12 l3 white">
-                        <div class="icon-block">
-                            <h2 class="center red-text"><i class="material-icons large">description</i></h2>
-                            <h4 class="center red white-text col-content">Scholarship</h4>
-                            <p>
-                                Barton did feebly change man she afford square add. Want eyes by neat so just must. Past draw tall up face show rent oh mr. Required is debating extended wondered as do. New get described applauded incommode shameless out extremity but. Resembled at perpetual no believing is otherwise sportsman. Is do he dispatched cultivated travelling astonished. Melancholy am considered possession on collecting everything.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col s12 l3 white">
-                        <div class="icon-block">
-                            <h2 class="center red-text"><i class="material-icons large">view_list</i></h2>
-                            <h4 class="center red white-text col-content">Service</h4>
-                            <p>
-                                Barton did feebly change man she afford square add. Want eyes by neat so just must. Past draw tall up face show rent oh mr. Required is debating extended wondered as do. New get described applauded incommode shameless out extremity but. Resembled at perpetual no believing is otherwise sportsman. Is do he dispatched cultivated travelling astonished. Melancholy am considered possession on collecting everything.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col s12 l3 white">
-                        <div class="icon-block">
-                            <h2 class="center red-text"><i class="material-icons large">grade</i></h2>
-                            <h4 class="center red white-text col-content">Leadership</h4>
-                            <p>
-                                Barton did feebly change man she afford square add. Want eyes by neat so just must. Past draw tall up face show rent oh mr. Required is debating extended wondered as do. New get described applauded incommode shameless out extremity but. Resembled at perpetual no believing is otherwise sportsman. Is do he dispatched cultivated travelling astonished. Melancholy am considered possession on collecting everything.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        </header>
+        <main>
+            <div class="container">
+                <h1 id="animated-typing" class="red-text">Welcome Ganesh Valliappan!</h1>
             </div>
-        </div>
+        </main>
 
-        <div class="parallax-container">
-            <div class="parallax"><img src="https://wallpaperscraft.com/image/crowd_people_dance_silhouette_57172_3840x2160.jpg"></div>
-        </div>
+        <?php include("footer.php"); ?>
 
-        <div class="container">
-            <div class="section">
-                <div class="row">
-                    <div class="col s12 m6">
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div id="login" class="modal">
-            <form class="row">
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">email</i>
-                    <input id="email" type="text" class="validate">
-                    <label for="email">Email</label>
-                </div>
-                <div class="input-field col s12">
-                    <i class="material-icons prefix">label</i>
-                    <input id="pass" type="password" class="validate">
-                    <label for="pass">Password</label>
-                </div>
-                <div style="text-align: center;">
-                    <button class="btn-large waves-effect waves-light center-align" type="submit" name="action" style="margin: auto;">Login
-                        <i class="material-icons left">send</i>
-                    </button>
-                </div>
-            </form>
-        </div>
-
-        <div id="register" class="modal">
-            <p>Swag Google Form</p>
-        </div>
-
-        <!-- JS -->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="js/materialize.min.js"></script>
-
-        <script type="text/javascript">
-            $(document).ready(function(){
-                $('.parallax').parallax();
-                $('.modal-trigger').leanModal({
-                    dismissible: true, // Modal can be dismissed by clicking outside of the modal
-                    opacity: .9, // Opacity of modal background
-                    in_duration: 300, // Transition in duration
-                    out_duration: 200, // Transition out duration
-                });
-                $(".button-collapse").sideNav();
-            });
-        </script>
+        <?php include("script.php"); ?>
     </body>
 </html>
--- script.php
+++ script.php
@@ -1,8 +1,10 @@
 <!-- JS -->
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/typed.min.js"></script>
 
 <script type="text/javascript">
+    var welcome = ["Hi Ganesh Valliappan!<br> ^2000Welcome to the NHS website"];
     $(document).ready(function(){
         $('.parallax').parallax();
         $('.modal-trigger').leanModal({
@@ -12,6 +14,10 @@
             out_duration: 200, // Transition out duration
         });
         $(".button-collapse").sideNav();
+        $(".element").typed({
+            strings: welcome,
+            typeSpeed: 0
+        });
     });
     $('.slider').slider({full_width: true});
 </script>
