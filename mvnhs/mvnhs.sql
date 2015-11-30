ec22457e2334b78e6636e27514130c646bcb2aba
Ganesh Valliappan <ganeshv757@gmail.com>
2015-11-29 18:43:50 -0800

    same

--- mvnhs/mvnhs.sql
+++ mvnhs/mvnhs.sql
@@ -9,7 +9,12 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Data exporting was unselected.
+-- Dumping data for table mvnhs.users: ~2 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`ID`, `fname`, `lname`, `grade`, `so1`, `so2`, `ju1`, `ju2`, `se1`, `se2`, `start_sem`, `email`, `pass`, `admin`) VALUES
+	(1, 'Ganesh', 'Valliappan', 11, 5, 6, 7, 8, 7, 8, 1, 'ganeshv757@gmail.com', 'valliappan', 1),
+	(2, 'Mac', 'Linerven', 12, 5, 5, 5, 5, 5, 5, 2, 'xdtooproxd@gmail.com', 'linerven', 0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
