4d51113c68b844655c4dcd690ed862125179f425
Ganesh Valliappan <ganeshv757@gmail.com>
2015-11-29 18:55:56 -0800

    more of the same

--- mvnhs/mvnhs.sql
+++ mvnhs/mvnhs.sql
@@ -9,12 +9,33 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Dumping data for table mvnhs.users: ~2 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`ID`, `fname`, `lname`, `grade`, `so1`, `so2`, `ju1`, `ju2`, `se1`, `se2`, `start_sem`, `email`, `pass`, `admin`) VALUES
-	(1, 'Ganesh', 'Valliappan', 11, 5, 6, 7, 8, 7, 8, 1, 'ganeshv757@gmail.com', 'valliappan', 1),
-	(2, 'Mac', 'Linerven', 12, 5, 5, 5, 5, 5, 5, 2, 'xdtooproxd@gmail.com', 'linerven', 0);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping database structure for mvnhs
+DROP DATABASE IF EXISTS `mvnhs`;
+CREATE DATABASE IF NOT EXISTS `mvnhs` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `mvnhs`;
+
+
+-- Dumping structure for table mvnhs.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(11) DEFAULT NULL,
+  `fname` tinytext,
+  `lname` tinytext,
+  `grade` int(11) DEFAULT NULL,
+  `so1` int(11) DEFAULT NULL,
+  `so2` int(11) DEFAULT NULL,
+  `ju1` int(11) DEFAULT NULL,
+  `ju2` int(11) DEFAULT NULL,
+  `se1` int(11) DEFAULT NULL,
+  `se2` int(11) DEFAULT NULL,
+  `start_sem` int(11) DEFAULT NULL,
+  `email` text,
+  `pass` text,
+  `admin` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
